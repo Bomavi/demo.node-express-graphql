ac2b9ca108544907c64cab20f004c48bdb4dcef7
Bomavi <max.bozh.n1mbo@gmail.com>
2019-11-30 23:41:55 +0200

    Update root with TS

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
-/* npm imports: common */
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+/* npm imports */
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+// import 'module-alias/register';
 
-/* Declare global rootRequire for root imports */
-global.rootRequire = name => require(`${__dirname}/${name}`);
-
-/* root imports: common */
-const { logger } = rootRequire('utils');
-const { api } = rootRequire('routes');
-const {
-	service,
-	mongoConnect,
-	redis: { session, redisOptions, redisStore },
-} = rootRequire('config');
+/* root imports */
+import { logger } from '~/utils';
+import { api } from '~/routes';
+import { service, mongoConnect, redis } from '~/config';
 
 /* Get .env constants */
 dotenv.config();
@@ -21,18 +15,18 @@ dotenv.config();
 /* Initialize microservice with credentials */
 service.init();
 
-// const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 const PORT = process.env.PORT || process.env.DEV_PORT;
 const maxAge = Number(process.env.SESSION_EXPIRES_IN) * 1000;
-const app = express();
+const app: Application = express();
 
 /* Initialize middlewares and express-session with REDIS */
 app.set('trust proxy', true);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
-	session({
-		store: new redisStore(redisOptions),
+	redis.session({
+		store: new redis.redisStore(redis.redisOptions),
 		secret: process.env.SESSION_SECRET,
 		resave: true,
 		rolling: false,
@@ -44,8 +38,7 @@ app.use(
 			maxAge,
 			httpOnly: true,
 			sameSite: true,
-			// secure: isProd,
-			secure: false,
+			secure: isProd,
 		},
 	})
 );
