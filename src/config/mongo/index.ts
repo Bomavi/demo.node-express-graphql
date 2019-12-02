68396c016fc5c916e894d0817df380c7223957e3
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-02 20:51:28 +0200

    Update mongo connection for typeorm

--- src/config/mongo/index.ts
+++ src/config/mongo/index.ts
@@ -1,38 +1,20 @@
 /* npm imports */
-import { connect, connection } from 'mongoose';
+import { createConnection, Connection } from 'typeorm';
 
-/* root imports */
-import { logger } from '~/utils';
+const mongoConnect = async (): Promise<Partial<Connection>> => {
+	const isProd = process.env.NODE_ENV === 'production';
+	const MONGO_DB_HOST = process.env.MONGO_DB_HOST || process.env.MONGO_DB_DEV_HOST;
+	const MONGO_DB_PORT = Number(
+		process.env.MONGO_DB_PORT || process.env.MONGO_DB_DEV_PORT
+	);
+	const DB_NAME = process.env.DB_NAME || process.env.DB_DEV_NAME;
 
-const MONGO_DB_HOST = process.env.MONGO_DB_HOST || process.env.MONGO_DB_DEV_HOST;
-const MONGO_DB_PORT = process.env.MONGO_DB_PORT || process.env.MONGO_DB_DEV_PORT;
-const DB_NAME = process.env.DB_NAME || process.env.DB_DEV_NAME;
-const connectionUrl = `${MONGO_DB_HOST}:${MONGO_DB_PORT}/${DB_NAME}`;
-const options = {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-};
-
-const mongoConnect = (): void => {
-	connect(connectionUrl, options);
-
-	connection.on('connected', () => {
-		logger.mongo(`Mongoose connection is open to: ${connectionUrl}`);
-	});
-
-	connection.on('error', e => {
-		logger.mongo(`Mongoose connection has occured ${e} error`);
-	});
-
-	connection.on('disconnected', () => {
-		logger.mongo('Mongoose connection was disconnected!');
-	});
-
-	process.on('SIGINT', () => {
-		connection.close(() => {
-			process.exit(0);
-		});
+	return await createConnection({
+		type: 'mongodb',
+		host: MONGO_DB_HOST,
+		port: MONGO_DB_PORT,
+		database: DB_NAME,
+		logging: !isProd,
 	});
 };
 
