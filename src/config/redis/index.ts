9ff5bd5f80308bfcbe0a65907ca35bc580715c78
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-02 22:33:02 +0200

    Move redis to its config file and update its config

--- src/config/redis/index.ts
+++ src/config/redis/index.ts
@@ -1,32 +1,50 @@
-/* npm imports */
 import session from 'express-session';
-import { createClient, ClientOpts } from 'redis';
-import connectRedis from 'connect-redis';
+import Redis, { RedisOptions } from 'ioredis';
+import connectRedis, { RedisStoreOptions } from 'connect-redis';
 
-/* root imports */
 import { logger } from '~/utils';
+
+const isProd = process.env.NODE_ENV === 'production';
+const maxAge = Number(process.env.SESSION_EXPIRES_IN) * 1000;
 
 const redisStore = connectRedis(session);
 
-const redisClientOptions: ClientOpts = {
+const redisClientOptions: RedisOptions = {
 	host: String(process.env.REDIS_HOST) || '127.0.0.1',
 	port: Number(process.env.REDIS_PORT) || 6379,
 };
 
-const redisClient = createClient(redisClientOptions);
+const redis = new Redis(redisClientOptions);
 
-const redisOptions = {
-	client: redisClient,
+const redisOptions: RedisStoreOptions = {
+	client: redis,
 };
 
-redisClient.on('ready', () => {
+const redisSessionMiddleware = session({
+	store: new redisStore(redisOptions),
+	secret: String(process.env.SESSION_SECRET),
+	resave: true,
+	rolling: false,
+	saveUninitialized: false,
+	proxy: true,
+	cookie: {
+		path: '/',
+		domain: 'localhost',
+		maxAge,
+		httpOnly: true,
+		sameSite: true,
+		secure: isProd,
+	},
+});
+
+redis.on('ready', () => {
 	logger.redis(
 		`Redis connection is open to: ${redisClientOptions.host}:${redisClientOptions.port}`
 	);
 });
 
-redisClient.on('error', err => {
+redis.on('error', err => {
 	logger.redis('Redis connection has occured error: %O', err);
 });
 
-export const redis = { session, redisOptions, redisStore };
+export { redisSessionMiddleware };
