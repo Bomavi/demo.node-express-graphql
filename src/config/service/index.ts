60c7959acf5e5153b418b4fb46867b6eaa14103d
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-02 15:22:17 +0200

    Update service initialization with TS

---
+++ src/config/service/index.ts
@@ -0,0 +1,15 @@
+/* root imports */
+import { jwt, logger } from '~/utils';
+
+const init = async (): Promise<void> => {
+	const serviceName = process.env.SERVICE_NAME || 'service';
+	const token = (await jwt.issue({ service: serviceName }, false)) as string;
+
+	global.SERVICE_NAME = serviceName;
+	global.SERVICE_TOKEN = token;
+
+	logger.app('SERVICE_NAME: %o', global.SERVICE_NAME);
+	logger.app('SERVICE_TOKEN: %o', global.SERVICE_TOKEN);
+};
+
+export const service = { init };
