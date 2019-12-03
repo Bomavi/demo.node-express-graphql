e85346064138c680620cac5181abac783f79e812
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-03 22:48:38 +0200

    Add authChecker middleware for type-graphql & remove others

---
+++ src/middleware/authChecker/index.ts
@@ -0,0 +1,36 @@
+import { AuthChecker } from 'type-graphql';
+import createError from 'http-errors';
+
+import { jwt } from '~/utils';
+
+export const authChecker: AuthChecker<ApolloContext, string[]> = async (
+	{ context: { req } },
+	roles
+) => {
+	let token = '';
+
+	if (req.session!.accessToken) {
+		token = req.session!.accessToken;
+	} else {
+		const accessToken = req.headers['authorization'];
+
+		if (!accessToken) throw createError(401, 'no accessToken provided!');
+
+		if (accessToken.startsWith('Bearer ')) {
+			const tokenData = accessToken.split(' ');
+			token = tokenData[1];
+		}
+	}
+
+	if (!token) throw createError(401, 'no accessToken provided!');
+
+	const decodedToken = await jwt.validate(token);
+
+	if (!decodedToken.userId) throw createError(403, 'token invalid!');
+
+	if (roles) {
+		// check for accepted roles
+	}
+
+	return true;
+};
