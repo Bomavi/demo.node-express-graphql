ebf2277f2a96f5e7266e937478fc49d561443099
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-08 23:15:25 +0200

    Add debug typings

--- src/utils/logger/debug-logger.ts
+++ src/utils/logger/debug-logger.ts
@@ -1,30 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import debugLogger from 'debug';
 
-const debug = (msg: string, format = ''): void => {
+const debug = (msg: string, format: any = ''): void => {
 	debugLogger('debug')(msg, format);
 };
 
-const app = (msg: string, format = ''): void => {
+const app = (msg: string, format: any = ''): void => {
 	debugLogger('app')(msg, format);
 };
 
-const api = (msg: string, format = ''): void => {
+const api = (msg: string, format: any = ''): void => {
 	debugLogger('api')(msg, format);
 };
 
-const http = (msg: string, format = ''): void => {
+const http = (msg: string, format: any = ''): void => {
 	debugLogger('http')(msg, format);
 };
 
-const mongo = (msg: string, format = ''): void => {
+const mongo = (msg: string, format: any = ''): void => {
 	debugLogger('mongo')(msg, format);
 };
 
-const redis = (msg: string, format = ''): void => {
+const redis = (msg: string, format: any = ''): void => {
 	debugLogger('redis')(msg, format);
 };
 
-const error = (msg: string, format = ''): void => {
+const error = (msg: string, format: any = ''): void => {
 	debugLogger('error')(msg, format);
 };
 
